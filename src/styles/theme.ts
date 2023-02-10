1a3b9473eb5f145baec8f3790999c408e0305faf
elrina <elrinaang1004@gmail.com>
2023-02-10 21:53:15 +0800

    Added font styling

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { caveat, poppins, raleway } from "@/pages/_app";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    typography: {
+      fontFamily: poppins.style.fontFamily,
+    },
+  })
